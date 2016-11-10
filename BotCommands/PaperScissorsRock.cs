e53c90d23b72afbd70d802c3bc67b8dfc96014a2
Andrew Dolan <ajd185@uclive.ac.nz>
2016-11-11 00:10:56 +1300

    Added game - paper, scissors, rock. Note: Reaper doesn't play fair.

---
+++ BotCommands/PaperScissorsRock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reaperbot.BotCommands
+{
+    class PaperScissorsRock : BotCommand
+    {
+        public PaperScissorsRock()
+        {
+            Paper();
+            Scissors();
+            Rock();
+            All();
+        }
+
+        // Paper
+        private void Paper()
+        {
+            commands.CreateCommand("paper")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendIsTyping();
+                    await e.Channel.SendMessage("I chose scissors: YOU LOSE PEASANT!");
+                });
+        }
+
+        // Scissors
+        private void Scissors()
+        {
+            commands.CreateCommand("scissors")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendIsTyping();
+                    await e.Channel.SendMessage("I chose rock: YOU LOSE PEASANT!");
+                });
+        }
+
+        // Rock
+        private void Rock()
+        {
+            commands.CreateCommand("rock")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendIsTyping();
+                    await e.Channel.SendMessage("I chose paper: YOU LOSE PEASANT!");
+                });
+        }
+
+        // All
+        private void All()
+        {
+            commands.CreateCommand("paperscissorsrock")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendIsTyping();
+                    await e.Channel.SendMessage("Let me start, I'll choose scissors!");
+                    System.Threading.Thread.Sleep(2000);
+                    await e.Channel.SendIsTyping();
+                    await e.Channel.SendMessage("Oh wait...");
+                });
+        }
+    }
+}
